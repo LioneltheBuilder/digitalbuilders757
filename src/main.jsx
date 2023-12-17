8f83efeb8f607d124effbac38c56a9ccd046beb9
Lionelthebuilder <younglionpics@gmail.com>
2023-12-17 16:10:16 -0500

    "updated main"
    Signed-off-by: Lionelthebuilder <younglionpics@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,68 +1,103 @@
 import React from 'react';
 import { StrictMode } from 'react';
-import { createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import logo from './images/logo.png';
 
 
-import logo from './images/logo.png'; 
+const items = [
+    {
+        name: "Sleep 8 hrs",
+        streak: 5,
+        id: 1
+    },
+    {
+        name: "Write anything",
+        streak: 5,
+        id: 2
+    },
+    {
+        name: "Meditate / Pray",
+        streak: 5,
+        id: 3
+    },
+    {
+        name: "Exercise",
+        streak: 5,
+        id: 4
+    },
+    {
+        name: "Cold Shower",
+        streak: 5,
+        id: 5
 
+    },
+    {
+        name: "Work on a Goal",
+        streak: 5,
+        id: 6
+    }
+
+]
 
 
 
 const Header = (props) => {
     return (
         <header>
-         <img src={logo} alt="Logo" style={{ height: '50px' }} /> 
+            <img src={logo} alt="Logo" style={{ height: '50px' }} />
             <h1>{props.title}</h1>\
             <span className='total-items'>Items: {props.itemTotal}</span>
         </header>
     )
 }
 
-    const Item = (props) => {
-        return (
-            <div className='item'>
-                <button className='remove-item'/>
-                <span className='item-name'>{props.name}</span>
-                <Counter quantity={props.streak} />
- 
-            </div>
-        )
-    }
+const Item = (props) => {
+    return (
+        <div className='item'>
+            <button className='remove-item' />
+            <span className='item-name'>{props.name}</span>
+            <Counter quantity={props.streak} />
 
-    const Counter = () => {
-        return (
-            <div className='quantity'>
+        </div>
+    )
+}
+
+const Counter = () => {
+    return (
+        <div className='quantity'>
             <span className='qty-label'> Streak</span>
             <button className='increment'>+</button>
             <button className='decrement'>-</button>
             <span className='quantity-amount'>1</span>
         </div>
 
-        )
-    }
+    )
+}
 
-    const App = () => {
-        return (
-            <div className ='todo-list'>
-                <Header 
-                    title='The 7 Paths' 
-                    itemTotal={7} />
+const App = (props) => {
+    return (
+        <div className='todo-list'>
+            <Header
+                title='The 7 Paths'
+                itemTotal={props.initialList.length} />
 
-                {/* To do List */}
-                <Item name="Sleep 8 hrs" streak={5} />
-                <Item name="Write anything" streak={5} />
-                <Item name="Meditate / Pray" streak={5} />
-                <Item name="Exercise" streak={5} />
-                <Item name="Cold Shower" streak={5} />
-                <Item name="Work on a Goal" streak={5} />
-            </div>
-        )
-    }
+            {/* 7 Paths */}
+            {props.initialList.map(item =>
+                <Item
+                    name={item.name}
+                    streak={item.quantity}
+                    key={item.id}
 
-    const root = createRoot(document.getElementById('root'));
-    root.render(
-        <StrictMode>
-        <App/> 
-        </StrictMode>
+                />
+            )}
+        </div>
+    )
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <App initialList={items} />
+    </StrictMode>
 
 );
