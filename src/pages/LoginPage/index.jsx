2cb029680445db2e408c29e4118df031cae673a8
Lionelthebuilder <younglionpics@gmail.com>
2023-12-30 23:08:42 -0500

    working on the front page
    Signed-off-by: Lionelthebuilder <younglionpics@gmail.com>

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,15 +1,15 @@
-
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import { auth } from '../../firebase'; // Ensure the path is correct
+import { auth } from '../../firebase';
 import { Box, Button, Input, Flex, Heading, Text, Image, useMediaQuery } from '@chakra-ui/react';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const [isSmallScreen] = useMediaQuery("(max-width: 480px)");
+  
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
@@ -37,46 +37,45 @@ const LoginPage = () => {
   };
 
   return (
-    <Flex direction="column" height="100vh">
+    <Flex direction="column" height="100vh" overflowY="auto">
       {/* Header */}
       <Box width="full" p={4} borderBottom="1px solid" borderColor="gray.200">
         <Flex justify="space-between" align="center">
-          <Heading size={isMobile ? "lg" : "md"}>The Digital Builders</Heading>
-          <Button onClick={handleJoinNow} colorScheme="blue">Join Now</Button>
+          <Heading size={isSmallScreen ? "sm" : "md"}>The Digital Builders</Heading>
+          <Button onClick={handleJoinNow} colorScheme="blue" size={isSmallScreen ? "sm" : "md"}>Join Now</Button>
         </Flex>
       </Box>
 
       {/* Hero Section */}
       <Flex
         flex={1}
-        p={10}
+        p={isSmallScreen ? 4 : 10}
+        direction={isSmallScreen ? "column-reverse" : "row"}
         align="center"
         justify="center"
-        direction={isMobile ? "column" : "row"}
       >
-        {/* Conditional Image Placement for Mobile */}
-        {isMobile && (
-          <Box mb={4}>
-            <Image src="/path-to-your-hero-image.jpg" alt="Hero Image" objectFit="cover" borderRadius="md" />
-          </Box>
-        )}
-
         {/* Left Side: Bold Statement and Tagline */}
-        <Flex direction="column" flex={1} justify="center" align="flex-start" pr={isMobile ? 0 : 10} textAlign={isMobile ? "center" : "left"}>
-          <Heading size={isMobile ? "xl" : "2xl"} mb={4}>Empower Your Tech Journey</Heading>
-          <Text fontSize={isMobile ? "md" : "xl"} mb={4}>Join us and start transforming your future today.</Text>
+        <Flex
+          direction="column"
+          flex={1}
+          justify="center"
+          align={isSmallScreen ? "center" : "flex-start"}
+          textAlign={isSmallScreen ? "center" : "left"}
+          mb={isSmallScreen ? 4 : 0}
+        >
+          <Heading size={isSmallScreen ? "lg" : "2xl"} mb={4}>Empower Your Tech Journey</Heading>
+          <Text fontSize={isSmallScreen ? "sm" : "xl"} mb={4}>Join us and start transforming your future today.</Text>
+          
           {/* Login Form */}
-          <Box maxWidth={isMobile ? "100%" : "400px"}>
+          <Box maxWidth={isSmallScreen ? "full" : "400px"}>
             {/* ... (existing login form code) */}
           </Box>
         </Flex>
 
-        {/* Right Side: Image Placeholder for Desktop */}
-        {!isMobile && (
-          <Box flex="1">
-            <Image src="/hero2.png" alt="Hero Image" objectFit="cover" borderRadius="md" />
-          </Box>
-        )}
+        {/* Right Side: Image Placeholder */}
+        <Box flex={1} minWidth={isSmallScreen ? "0" : "300px"} width={isSmallScreen ? "100%" : "auto"}>
+          <Image src="/hero2.png" alt="Hero Image" objectFit="cover" borderRadius="md" />
+        </Box>
       </Flex>
     </Flex>
   );
